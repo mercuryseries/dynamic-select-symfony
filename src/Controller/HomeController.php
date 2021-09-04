a83548f162b854abba9f2b87d9baa7e00b819de9
Honore Hounwanou <mercuryseries@gmail.com>
2021-09-04 09:08:07 -0400

    Add fixtures for countries and cities

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CityRepository;
+use App\Repository\CountryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CityRepository $cityRepository, CountryRepository $countryRepository): Response
     {
+        dd($cityRepository->findAll(), $countryRepository->findAll());
+
         return $this->render('home.html.twig');
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\City;
+use App\Entity\Country;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,68 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // France
+        $france = new Country;
+        $france->setName('France');
+
+        $paris = new City;
+        $paris->setName('Paris');
+        $paris->setCountry($france);
+
+        $marseille = new City;
+        $marseille->setName('Marseille');
+        $marseille->setCountry($france);
+
+        $lyon = new City;
+        $lyon->setName('Lyon');
+        $lyon->setCountry($france);
+
+        $manager->persist($france);
+        $manager->persist($paris);
+        $manager->persist($marseille);
+        $manager->persist($lyon);
+
+        // Canada
+        $canada = new Country;
+        $canada->setName('Canada');
+
+        $quebec = new City;
+        $quebec->setName('Québec');
+        $quebec->setCountry($canada);
+
+        $montreal = new City;
+        $montreal->setName('Montréal');
+        $montreal->setCountry($canada);
+
+        $troisRivieres = new City;
+        $troisRivieres->setName('Trois-Rivières');
+        $troisRivieres->setCountry($canada);
+
+        $manager->persist($canada);
+        $manager->persist($quebec);
+        $manager->persist($montreal);
+        $manager->persist($troisRivieres);
+
+        // Côte d'ivoire
+        $coteDivoire = new Country;
+        $coteDivoire->setName("Côte d'ivoire");
+
+        $abidjan = new City;
+        $abidjan->setName('Abidjan');
+        $abidjan->setCountry($canada);
+
+        $yamoussoukro = new City;
+        $yamoussoukro->setName('Yamoussoukro');
+        $yamoussoukro->setCountry($canada);
+
+        $bouake = new City;
+        $bouake->setName('Bouaké');
+        $bouake->setCountry($canada);
+
+        $manager->persist($coteDivoire);
+        $manager->persist($abidjan);
+        $manager->persist($yamoussoukro);
+        $manager->persist($bouake);
 
         $manager->flush();
     }
