beae4b06d58f0a074078f67fda53a0fbcccc6496
Honore Hounwanou <mercuryseries@gmail.com>
2021-09-04 08:49:07 -0400

    Add homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home.html.twig');
     }
 }
